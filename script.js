3c823bc2c324d809950182d063563c403a84a56b
Abdimalik-dev <cabdixuseen076@gmail.com>
2023-10-14 22:33:00 +0300

    commit

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello");
+const grade = 85;
+
+if (grade >= 90 && 100) {
+    console.log('you got an A grade');
+} else if (grade >= 70 && 80 ){
+    console.log('You got an B grade');
+}else if  (grade >= 50 && 60 ){
+    console.log('You got C grade');
+}  else if  (grade <= 45) {
+    console.log('You got an Failed');
+}
